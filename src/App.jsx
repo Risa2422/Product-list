c900f54b07b78235d0582cfce513de920675e784
Risa Yamamoto <yamari2422@gmail.com>
2024-09-17 23:14:19 -0700

    Commit : All materials

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, createContext } from "react";
+import "./App.css";
+import CardLists from "./components/CardLists/CardLists";
+import Cart from "./components/Cart/Cart";
+export const MyContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [isInCart, setIsInCart] = useState({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false,
+    9: false,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyContext.Provider
+        value={{ selectedItems, setSelectedItems, isInCart, setIsInCart }}
+      >
+        <CardLists />
+        <Cart />
+      </MyContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
