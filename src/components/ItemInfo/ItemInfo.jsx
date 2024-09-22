dde32fd94de4b1bc6f4657562fdc6cad8b01a773
Risa Yamamoto <yamari2422@gmail.com>
2024-09-21 21:11:49 -0700

    Deleted : the ConfirmButton components and added the code to the ItemInfo.jsx file

--- src/components/ItemInfo/ItemInfo.jsx
+++ src/components/ItemInfo/ItemInfo.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProductContext from "../../context/ProductContext";
 import OrderLists from "../OrderLists/OrderLists";
-import ConfirmButton from "../ConfirmButton/ConfirmButton";
+import ConfirmModal from "../ConfirmModal/ConfirmModal";
 
 const ItemInfo = () => {
   const { selectedItems, setSelectedItems } = useContext(ProductContext);
+  const [showOrderLists, setShowOrderLists] = useState(false);
+
+  const handleClick = () => {
+    setShowOrderLists(true);
+  };
 
   return (
     <div>
@@ -16,7 +21,8 @@ const ItemInfo = () => {
           <div>
             This is a <strong>carbon-neutral</strong> delivery
           </div>
-          <ConfirmButton />
+          <button onClick={handleClick}>Confirm</button>
+          {showOrderLists && <ConfirmModal />}
         </>
       )}
     </div>
